1b2609c9f9fd2a9f330e242fe5ffdc32e9381545
wakelawn <islamraj477@gmail.com>
2026-06-11 14:28:48 +0530

    Changes made regarding the js file logic error

--- script.js
+++ script.js
@@ -5,38 +5,31 @@ let num1 = 5;
 let num2 = 10;
 let num3 = 15;
 let num4 = 20;
-let num5 = 25;
-let num6 = 30;
 
-if (num1 % 2) {
-  console.log("The number", num1, " is even. ");
+// Checking num1
+if (num1 % 2 === 0) {
+  console.log("The number " + num1 + " is even.");
 } else {
-  console.log("The number", num1, "is odd.");
+  console.log("The number " + num1 + " is odd.");
 }
 
-if (num2 % 2) {
-  console.log("The number", num2, " is even. ");
+// Checking num2
+if (num2 % 2 === 0) {
+  console.log("The number " + num2 + " is even.");
 } else {
-  console.log("The number", num2, "is odd.");
+  console.log("The number " + num2 + " is odd.");
 }
-if (num3 % 2) {
-  console.log("The number", num3, " is even. ");
+
+// Checking num3
+if (num3 % 2 === 0) {
+  console.log("The number " + num3 + " is even.");
 } else {
-  console.log("The number", num3, "is odd.");
+  console.log("The number " + num3 + " is odd.");
 }
-if (num4 % 2) {
-  console.log("The number", num4, " is even. ");
-  6;
+
+// Checking num4
+if (num4 % 2 === 0) {
+  console.log("The number " + num4 + " is even.");
 } else {
-  console.log("The number", num4, "is odd.");
-}
-if (num5 % 2) {
-  console.log("The number", num5, " is even. ");
-} else {
-  console.log("The number", num5, "is odd.");
-}
-if (num6 % 2) {
-  console.log("The number", num6, " is even. ");
-} else {
-  console.log("The number", num6, "is odd.");
+  console.log("The number " + num4 + " is odd.");
 }
