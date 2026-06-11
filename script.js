f6ed22e80c59c55439fd7a2a25207378c69fddbe
wakelawn <islamraj477@gmail.com>
2026-06-11 17:09:29 +0530

    chnages in the js file

--- script.js
+++ script.js
@@ -1,35 +1,12 @@
-// The objective of this assignment is to create a JavaScript program that
-// checks whether a given number is even or odd.
-
-let num1 = 5;
-let num2 = 10;
-let num3 = 15;
-let num4 = 20;
-
-// Checking num1
-if (num1 % 2 === 0) {
-  console.log("The number " + num1 + " is even.");
-} else {
-  console.log("The number " + num1 + " is odd.");
-}
-
-// Checking num2
-if (num2 % 2 === 0) {
-  console.log("The number " + num2 + " is even.");
-} else {
-  console.log("The number " + num2 + " is odd.");
-}
-
-// Checking num3
-if (num3 % 2 === 0) {
-  console.log("The number " + num3 + " is even.");
-} else {
-  console.log("The number " + num3 + " is odd.");
-}
-
-// Checking num4
-if (num4 % 2 === 0) {
-  console.log("The number " + num4 + " is even.");
-} else {
-  console.log("The number " + num4 + " is odd.");
-}
+// Tasks:
+//  Write a JavaScript program that performs the following operations on a given
+// number:
+//  Find the sum of first n numbers where n is a variable.
+// Print table of n where n is a variable.
+// Check if it is a prime number.
+// Print all its factors.
+// Find sum of all digits of a number (139 = 13 => 1+3+9 =13).
+// Check if it is an Armstrong number.
+// - An Armstrong number is one whose sum of digits raised to the power three equals the number ( ex 153 => 1^3 + 5^3 + 3^3 = 153 )
+//  Use different variables for checking all the things
+// Display the results of each operation to the user using console.log.
