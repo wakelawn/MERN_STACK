b869e717caa0cf072c73e8196a708b631e3ab147
wakelawn <islamraj477@gmail.com>
2026-07-04 23:36:50 +0530

    protoype of the map/filter - test

--- script.js
+++ script.js
@@ -1,3 +1,79 @@
-function searching-studs(){
+const students = [
+    {
+        name:"Aarav Das",
+        marks:90,
+        class:"BCA",
+        address:"Mumbai"
+    },
+    {
+        name:"Bablu Kumar",
+        marks:98,
+        class:"BCA",
+        address:"Mumbai"
+    },
+    {
+        name:"Chati Dev",
+        marks:85,
+        class:"BCA",
+        address:"Delhi"
+    },
+    {
+        name:"Debasis Borah",
+        marks:74,
+        class:"BCA",
+        address:"Assam"
+    },
+    {
+        name:"Eiphoria Depsi",
+        marks:12,
+        class:"BCA",
+        address:"Goa"
+    },
+    {
+        name:"Farah Khan",
+        marks:45,
+        class:"BCA",
+        address:"Mumbai"
+    },
+    {
+        name:"Gaurav Sahu",
+        marks:85,
+        class:"BCA",
+        address:"Navi Mumbai"
+    },
+    {
+        name:"Hiran Lal Chadda",
+        marks:88,
+        class:"BCA",
+        address:"Punjab"
+    },
+    {
+        name:"Intiaz Ali",
+        marks:95,
+        class:"BCA",
+        address:"Gujrat"
+    },
+    {
+        name:"Jenifer Lawrence",
+        marks:25,
+        class:"BCA",
+        address:"Tamil Nadu"
+    },
+    {
+        name:"Kangana Singh",
+        marks:38,
+        class:"BCA",
+        address:"Jammu"
+    },
+    {
+        name:"Lahore Prasad",
+        marks:88,
+        class:"BCA",
+        address:"Punjab"
+    }
+];
 
-}
+
+
+
+
